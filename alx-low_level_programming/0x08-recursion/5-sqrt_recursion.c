b0d7783df4e21f443b4d783e9474efd7daa52e9a
Emmanuel Alabi <bobslegend795@gmail.com>
2023-07-06 00:13:29 +0100

    dd

---
+++ alx-low_level_programming/0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Find square root of a number
+ * @n: The number
+ *
+ * Return: The square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n <= 0)
+		return (-1);
+
+}
---
+++ alx-low_level_programming/0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * is_prime_number - Check if prime number
+ * @n: Number to check
+ * Return: 1 or 0
+ */
+int is_prime_number(int n)
+{
+	
+}
